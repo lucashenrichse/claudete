514e4dde1c5b794d39e7497b8ab6893ecdcc8d0e
lucashenrichse <127400839+lucashenrichse@users.noreply.github.com>
2024-08-07 09:06:24 -0300

    cleiton

--- script.js
+++ script.js
@@ -3,3 +3,38 @@ const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const texoResutado = document.querySelector('.texo-resultado');
+
+const perguntas = [
+    {
+        enunciado: "vc fala sozinho",
+        alternativas: [
+            "sim",
+            "nao"
+        ]
+    }
+
+]
+{
+    enunciado: "vc faz terapia",
+        alternativas[
+        "claro que não sou louco",
+        "alo caps"
+        ]
+};
+
+
+
+
+enunciado: "vc ja ",
+        alternativas[
+        "",
+        "Alternativa 2"
+        ]
+
+        {
+            enunciado: "vc ve coisas que nao estão lá",
+                alternativas[
+                "fantasma conta?",
+                "devo chamar um padre"
+                ]
+        };
